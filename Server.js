b269927cf411bd97ee02813edcf06bf3539b91d0
Carlinhoelpro <carloslizanamiranda@hotmail.com>
2025-11-03 19:18:55 -0300

    Se creó el servidor para que funciones la página con la base de datos y la api

---
+++ Server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'TallerTokyoNoodles',
+  database: 'taller_1'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err);
+    return;
+  }
+  console.log('Conectado correctamente a MySQL');
+});
+
+app.get('/', (req, res) => {
+  res.send('Servidor backend funcionando correctamente ');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
